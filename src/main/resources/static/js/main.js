1c22bb6f13e968fb3eb4e758469074623e80f5df
lirugo <roostik21@gmail.com>
2019-10-30 12:31:32 +0200

    Vuex Dark/Light switcher

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,9 +1,13 @@
 import App from './pages/App.vue'
 import Vue from 'vue'
 import vuetify from 'vuetify.js'
+import 'es6-promise/auto'
+import store from 'store.js'
+
 
 new Vue({
     el: '#app',
     vuetify,
+    store,
     render: a => a(App),
 })
