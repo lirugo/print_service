9e6fa7ee7fcc100cb7f3a812178fcea25c5f0df6
lirugo <roostik21@gmail.com>
2019-12-04 17:39:25 +0200

    Minor

--- src/main/java/com/lirugo/print_service/controller/MainController.java
+++ src/main/java/com/lirugo/print_service/controller/MainController.java
@@ -2,13 +2,13 @@ package com.lirugo.print_service.controller;
 
 import com.lirugo.print_service.entity.User;
 import com.lirugo.print_service.repo.UserRepo;
+import com.lirugo.print_service.service.auth.UserPrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.Optional;
 
 @Controller
 public class MainController {
@@ -18,9 +18,9 @@ public class MainController {
 
     @GetMapping("/")
     public String index(Model model, Authentication authentication){
-        Optional<User> user = userRepo.findById(authentication.getName());
+        User user = ((UserPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUser();
 
-        model.addAttribute("userData", user.get());
+        model.addAttribute("userData", user);
         return "index";
     }
 }
